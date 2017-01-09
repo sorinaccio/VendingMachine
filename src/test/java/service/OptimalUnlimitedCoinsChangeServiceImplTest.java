28cac5748fda94ff5f22c3fd9e34a44fd7f40f74
Sorinaccio <amzasorin@yahoo.com>
2017-01-09 08:32:03 +0200

    Adding first Unit Test

---
+++ src/test/java/service/OptimalUnlimitedCoinsChangeServiceImplTest.java
@@ -0,0 +1,20 @@
+package service;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by sorinaccio on 1/9/2017.
+ */
+public class OptimalUnlimitedCoinsChangeServiceImplTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testGetOptimalChangeFor() throws Exception {
+
+    }
+}
